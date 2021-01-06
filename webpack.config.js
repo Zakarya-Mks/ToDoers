98e925f5f25b40bb226534b16cbd31664bcbaee5
Zakarya-Mks <zakaaryaaa@gmail.com>
2021-01-06 18:38:37 +0100

    compress the main.js file

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/js/app.js',
-  mode: 'development',
+  mode: 'production',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
